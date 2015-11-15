36bc3b4fd8aba4694a1245c9d00574ceb9bd7429
如彼 <hua.qiuh@alibaba-inc.com>
2015-11-15 12:30:14 +0800

    improve eventmachine example

--- server/06_eventmachine_multiple_client_tcp_server.rb
+++ server/06_eventmachine_multiple_client_tcp_server.rb
@@ -9,14 +9,42 @@ class Server
     end
   end
 
-  def log(msg=nil, &block)
-    Logger.new(STDOUT).debug msg, &block
-  end
-
   module Handler
+
+    # Everytime a new client is connected, EventMachine will initialize
+    # a new Connection class (or a class inheriting from Connection defined
+    # by you). The instance of Connection class with automaticly invoke
+    # `post_init` method
     def post_init
-      log { "new client connected" }
+      @client_addr = client_addr
+      log { "new client connected from: %s:%d" % client_addr }
     end
+
+    # Everytime client send data to server, this method will be invoked
+    def receive_data data
+      data.chomp!
+      if data == "q"
+        close_connection
+      else
+        send_data data.reverse 
+        send_data "\n"
+      end
+    end
+
+    def unbind
+      log { "client disconnected from: %s:%d" % @client_addr }
+    end
+
+    private
+
+      def log(msg=nil, &block)
+        Logger.new(STDOUT).debug msg, &block
+      end
+
+      def client_addr
+        Socket.unpack_sockaddr_in(get_peername).reverse
+      end
+
   end
 
 end
