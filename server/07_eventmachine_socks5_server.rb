d9eb8f95b2195d1243d39cde6d3ffe615d5bbe70
如彼 <hua.qiuh@alibaba-inc.com>
2015-11-15 14:05:43 +0800

    refactor

--- server/07_eventmachine_socks5_server.rb
+++ server/07_eventmachine_socks5_server.rb
@@ -5,49 +5,60 @@ require 'stringio'
 class Server
 
   def start(bind: '0.0.0.0', port: nil)
-    EM.run do
-      EM.start_server bind, port, Socks5Handler
-    end
+    EM.run { EM.start_server bind, port, Socks5Handler }
   end
 
   # SOCKS5 proxy server
   # RFC#1928: https://www.ietf.org/rfc/rfc1928.txt
   module Socks5Handler
 
+    # we only support a few socks5 features for demo only
     SOCKS_GREETING       = "\x05\x02\x00\x01"
     SOCKS_GREETING_REPLY = "\x05\x00"
-    SOCKS_CONNECT        = "\x05\x01\x00\x03"
+    SOCKS_CONNECT        = "\x05\x01\x00"
     SOCKS_CONNECT_REPLY  = "\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00"
 
-    attr_accessor :dest_host, :dest_port
+    attr_accessor :dest_host, :dest_port, :proxy_conn, :data
 
     def post_init
       @client_addr = client_addr
       log { "new client connected from: %s:%d" % client_addr }
     end
 
-    # Everytime client send data to server, this method will be invoked
     def receive_data data
-      unless @proxy_conn
-        if data == SOCKS_GREETING
-          send_data SOCKS_GREETING_REPLY
-          return
-        end
-
-        io = StringIO.new(data)
-        if io.read(4) == SOCKS_CONNECT
-          len = io.getbyte
-          self.dest_host = io.read(len)
-          self.dest_port = io.read(2).unpack("n").first
-          log { "destination: %s:%d" % [dest_host, dest_port] }
-
-          start_proxy
-          send_data SOCKS_CONNECT_REPLY
-        end
-        return
+      @data = data
+      if proxy_ready?
+        proxy_conn.send_data data
+      else
+        greeting or connect
       end
+    end
 
-      @proxy_conn.send_data data
+    def proxy_ready?
+      !!proxy_conn
+    end
+
+    def greeting
+      send_data SOCKS_GREETING_REPLY if data == SOCKS_GREETING
+    end
+
+    def connect
+      io = StringIO.new(data)
+      if io.read(3) == SOCKS_CONNECT
+        self.dest_host =  case atype = io.getbyte
+                          when 1
+                            io.read(4).unpack("C*").join "."
+                          when 3
+                            io.read(io.getbyte)
+                          when 4
+                            io.read(16).unpack("n*").map {|i| i.to_s(16)}.join ":"
+                          end
+        self.dest_port = io.read(2).unpack("n").first
+        start_proxy
+
+        log { "destination connected: %s:%d" % [dest_host, dest_port] }
+        send_data SOCKS_CONNECT_REPLY
+      end
     end
 
     def start_proxy
@@ -58,12 +69,8 @@ class Server
           @client = client
         end
 
-        def post_init
-          EM.enable_proxy self, @client
-        end
-
-        def proxy_target_unbound
-          close_connection
+        def receive_data data
+          @client.send_data data
         end
 
         def unbind
